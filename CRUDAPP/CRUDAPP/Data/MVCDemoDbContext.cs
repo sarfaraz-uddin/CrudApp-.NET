3fa5b7a72978a11bde6669f6092b1b03ba0a275c
Sarfaraz <sarfarazuddin7860@gmail.com>
2023-09-03 21:48:15 +0545

    new commit

---
+++ CRUDAPP/CRUDAPP/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDAPP.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAPP.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
